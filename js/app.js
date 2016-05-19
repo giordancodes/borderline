81eeaa061ff3b00622ad4aff08e376c08e9d4a4f
Giordan Battaglin <gio@giordan.ca>
2016-05-19 19:10:12 -0400

    js starteed

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(function () {
+
+	var app = {};
+
+	app.inputOne = function () {
+		console.log('eyyyy');
+	};
+});
+
+$(document).ready(function () {
+	app.inputOne();
+});
---
+++ js/borderline.esnext.js
@@ -0,0 +1,24 @@
+$( ()=>	{
+	
+	
+	let app = {};
+	
+	
+	app.inputOne = ()=>{
+		console.log('eyyyy');
+	}
+	
+	
+});
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+	app.inputOne();
+});
