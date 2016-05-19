db8d54c52a5287d5768333ff559f9472e7e3e1ae
Giordan Battaglin <gio@giordan.ca>
2016-05-19 18:53:06 -0400

    more initial updates

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('bs', function() {
 	});
 });
 
-gulp.task('styles', function() {
+gulp.task('style', function() {
 	return gulp.src('./sass/**/*.scss')
 		.pipe(plumber({
 		  errorHandler: notify.onError("Error: <%= error.message %>")
@@ -28,7 +28,7 @@ gulp.task('styles', function() {
 		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(minifyCSS())
-		.pipe(concat('styles.css'))
+		.pipe(concat('style.css'))
 		.pipe(autoprefixer('last 5 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('./'))
@@ -53,9 +53,9 @@ gulp.task('images', function () {
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('sass/**/*.scss', ['style']);
 	gulp.watch('./js/**/*.js', ['app']);
 	gulp.watch('./**/*.html', reload);
 });
 
-gulp.task('default', ['styles', 'app', 'images', 'bs', 'watch']);
+gulp.task('default', ['style', 'app', 'images', 'bs', 'watch']);
